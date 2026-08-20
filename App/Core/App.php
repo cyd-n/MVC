b909c71ff4717a5552944a889a6ada39122c285c
Cyd'n <117258557+cyd-n@users.noreply.github.com>
2026-08-20 11:25:48 +0200

    Add a GET Reqeust

--- App/Core/App.php
+++ App/Core/App.php
@@ -8,73 +8,7 @@
         public function __construct(){
             require_once '../App/Core/Router.php';
             $router = new Router;
-            $router->Get("/Test", "Home@Index", []);
+            $router->Get("/Home", "Home@Index", []);
             $router->ReqeustMethode();
-
-           $url = $this->parseUrl();
-
-            if(file_exists('../App/Controllers/' . $url[0] . '.php') || $url[0] == null){
-                if(file_exists('../App/Controllers/' . $url[0] . '.php')){
-                    $this->controller = $url[0];
-                    unset($url[0]);
-                } else if(file_exists('../App/Controllers/' . $controller . '.php')){ }
-
-                require_once '../App/Controllers/' . $this->controller . '.php';
-
-                $this->controller = new $this->controller;
-
-                $methode_name = strtolower($url[1]);
-
-                if(isset($methode_name) || isset($method)){
-                    if(method_exists($this->controller, $methode_name)){
-                        $this->method = $methode_name;
-                        unset($url[1]);
-                    } else if(method_exists($this->controller, $this->method) && $url[1] == null){ 
-                        // skip
-                    } else {
-                        if (file_exists('../App/Controllers/ErrorController.php')) {
-                            $this->controller = 'ErrorController';
-
-                            require_once '../App/Controllers/' . $this->controller . '.php';
-
-                            $this->controller = new $this->controller;
-
-                            if (method_exists($this->controller, '_405')) {
-                                $this->method = '_405';
-                            }
-
-                            call_user_func_array([$this->controller, $this->method], []);
-                        } else {
-                            echo "Error 405";
-                        }
-                    }
-                }
-
-                $this->params = $url ? array_values($url) : [];
-
-                call_user_func_array([$this->controller, $this->method], $this->params);
-            } else { // Enter the 404 Error
-                if (file_exists('../App/Controllers/ErrorController.php')) {
-                    $this->controller = 'ErrorController';
-
-                    require_once '../App/Controllers/' . $this->controller . '.php';
-
-                    $this->controller = new $this->controller;
-
-                    if (method_exists($this->controller, '_404')) {
-                        $this->method = '_404';
-                    }
-
-                    call_user_func_array([$this->controller, $this->method], []);
-                } else {
-                    echo "Error 404";
-                }
-            }
-        }
-
-        public function parseUrl(){
-            if(isset($_GET['url'])) {
-                return $url = explode('/',filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL));
-            }
         }
     }
--- App/Core/Router.php
+++ App/Core/Router.php
@@ -27,11 +27,19 @@
             $this->urls[] = ["Methode" => $methode, "Url" => $url, "Handler" => $controller];
         }
 
-        public function ReqeustMethode(){
+        public function ReqeustMethode(){ // need to be able to use params
             $method = $_SERVER['REQUEST_METHOD'];
 
             if ($method === 'GET') {
-                echo "GET";
+                foreach($this->urls as $url){
+                    if($url['Methode'] == $method){
+                        $getUrl = '/' . $_GET['url'];
+
+                        if($url['Url'] == $getUrl) {
+                            $this->SetController($url['Handler']);
+                        }
+                    }
+                }
             } elseif ($method === 'POST') {
                 // POST request
             } elseif ($method === 'PUT') {
@@ -41,11 +49,10 @@
             }
         }
 
+        public function SetController($controller = ""){
+           $url = $this->ParseUrl($controller);
 
-       /* public function __construct(){
-           $url = $this->parseUrl();
-
-            if(file_exists('../App/Controllers/' . $url[0] . '.php') || $url[0] == null){
+           if(file_exists('../App/Controllers/' . $url[0] . '.php') || $url[0].trim() == ""){
                 if(file_exists('../App/Controllers/' . $url[0] . '.php')){
                     $this->controller = $url[0];
                     unset($url[0]);
@@ -57,28 +64,14 @@
 
                 $methode_name = strtolower($url[1]);
 
-                if(isset($methode_name) || isset($method)){
+                if(isset($methode_name) || isset($this->method)){
                     if(method_exists($this->controller, $methode_name)){
                         $this->method = $methode_name;
                         unset($url[1]);
                     } else if(method_exists($this->controller, $this->method) && $url[1] == null){ 
                         // skip
                     } else {
-                        if (file_exists('../App/Controllers/ErrorController.php')) {
-                            $this->controller = 'ErrorController';
-
-                            require_once '../App/Controllers/' . $this->controller . '.php';
-
-                            $this->controller = new $this->controller;
-
-                            if (method_exists($this->controller, '_405')) {
-                                $this->method = '_405';
-                            }
-
-                            call_user_func_array([$this->controller, $this->method], []);
-                        } else {
-                            echo "Error 405";
-                        }
+                        $this->Error405();
                     }
                 }
 
@@ -86,27 +79,51 @@
 
                 call_user_func_array([$this->controller, $this->method], $this->params);
             } else { // Enter the 404 Error
-                if (file_exists('../App/Controllers/ErrorController.php')) {
-                    $this->controller = 'ErrorController';
+                $this->Error404();
+            } 
+        }
 
-                    require_once '../App/Controllers/' . $this->controller . '.php';
+        public function ParseUrl($controller){
+            if(isset($controller)) {
+                return $url = explode('@',filter_var(rtrim($controller, '@'), FILTER_SANITIZE_URL));
+            }
+            
+            return "";
+        } 
 
-                    $this->controller = new $this->controller;
+        private function Error404(){
+            if (file_exists('../App/Controllers/ErrorController.php')) {
+                $this->controller = 'ErrorController';
 
-                    if (method_exists($this->controller, '_404')) {
-                        $this->method = '_404';
-                    }
+                require_once '../App/Controllers/' . $this->controller . '.php';
 
-                    call_user_func_array([$this->controller, $this->method], []);
-                } else {
-                    echo "Error 404";
+                $this->controller = new $this->controller;
+
+                if (method_exists($this->controller, '_404')) {
+                    $this->method = '_404';
                 }
+
+                call_user_func_array([$this->controller, $this->method], []);
+            } else {
+                echo "Error 404";
             }
         }
 
-        public function parseUrl(){
-            if(isset($_GET['url'])) {
-                return $url = explode('/',filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL));
+        private function Error405(){
+            if (file_exists('../App/Controllers/ErrorController.php')) {
+                $this->controller = 'ErrorController';
+
+                require_once '../App/Controllers/' . $this->controller . '.php';
+
+                $this->controller = new $this->controller;
+
+                if (method_exists($this->controller, '_405')) {
+                    $this->method = '_405';
+                }
+
+                call_user_func_array([$this->controller, $this->method], []);
+            } else {
+                echo "Error 405";
             }
-        } */
+        }
     }
